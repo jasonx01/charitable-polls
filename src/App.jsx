2711ae31a4bb87fef8f54fd6eb19939b9da73d62
mm11762 <MatthewMa929@Gmail.com>
2023-07-31 17:58:38 -0400

    Phone Screen after Jason changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,12 @@ import { useState } from 'react'
 import Home from './pages/HomePage.jsx'
 import Charity from './pages/CharityPage.jsx'
 import Search from './pages/SearchPage.jsx'
+import './global.css'
 import { Route, Routes, Link } from 'react-router-dom'
 
 function App() {
   return (
-    <>
+    <div className='phone-screen'>
       <ul>
         <li><Link to="/">Home</Link></li>
         <li><Link to="/search">Search</Link></li>
@@ -17,7 +18,7 @@ function App() {
         <Route path="/search" element={<Search />}></Route>
         <Route path="/charity" element={<Charity />}></Route>
       </Routes>
-    </>
+    </div>
   )
 }
 
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,3 @@
 export default function Home() {
-    return <h1>You are on the home page</h1>
+    return (<div>This is Home</div>)
 }
