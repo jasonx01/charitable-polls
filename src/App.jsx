2c576aa51cad8950ba21f9e4eedb461bcc43e3c4
William Lin <88000735+SoulSentinel@users.noreply.github.com>
2023-08-02 14:55:30 -0400

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,18 @@ import { useState } from 'react'
 import Home from './pages/HomePage.jsx'
 import Charity from './pages/CharityPage.jsx'
 import Search from './pages/SearchPage.jsx'
+import Poll from './pages/QuestionPage.jsx'
 import './global.css'
 import { Route, Routes, Link } from 'react-router-dom'
 
 function App() {
   return (
     <div className='phone-screen'>
-      {/* <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/search">Search</Link></li>
-        <li><Link to="/charity">Charity</Link></li>
-      </ul> */}
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/search" element={<Search />}></Route>
         <Route path="/charity" element={<Charity />}></Route>
+        <Route path="/poll" element={<Poll />}></Route>
       </Routes>
     </div>
   )
