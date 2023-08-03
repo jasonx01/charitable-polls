321adbe3bea45269b3146a98b20b965316ee3d3d
mm11762 <MatthewMa929@Gmail.com>
2023-08-03 16:54:42 -0400

    Some stuff

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,19 @@ import Search from './pages/SearchPage.jsx'
 import Poll from './pages/QuestionPage.jsx'
 import './global.css'
 import { Route, Routes, Link } from 'react-router-dom'
+import Iguana from './images/iguana.png'
 
 function App() {
   return (
     <div className='phone-screen'> 
-     {<ul> 
+     {/* {<ul> 
         <li><Link to="/">Home</Link></li>
         <li><Link to="/search">Search</Link></li>
         <li><Link to="/charity">Charity</Link></li>
         <li><Link to="/poll">Poll</Link></li>
-    </ul>}
+    </ul>} */}
       <Routes>
-        <Route path="/" element={<Home />}></Route>
+        <Route path="/" element={<Home iguanaImage={Iguana}/>}></Route>
         <Route path="/search" element={<Search />}></Route>
         <Route path="/charity" element={<Charity />}></Route>
         <Route path="/poll" element={<Poll />}></Route>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,18 +1,23 @@
-export default function Home() {
+
+export default function Home({iguanaImage}) {
     return (
     <div>
         <div className='title'>
             Charitable-Polls
         </div>
-        <div className='title'>
-            About
-        </div>
         <p>
-            There
+            Start Polling button
+        </p>
+        <p>
+            Followed Charities
         </p>
         <div className='charity-boxes-container'>
             <p className='charity-box'>
-
+                <img src={iguanaImage} alt='Iguana Image'></img>
+            </p>
+            <br></br>
+            <p className='charity-box'>
+                <img src={iguanaImage} alt='Iguana Image'></img>
             </p>
         </div>
     </div>
