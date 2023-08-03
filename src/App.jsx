ad664ac8a85eaeb730300fe38de8b0410760195d
jasonx01 <jaysonlau2003@gmail.com>
2023-08-02 21:35:15 -0400

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -4,23 +4,25 @@ import Charity from './pages/CharityPage.jsx'
 import Search from './pages/SearchPage.jsx'
 import Poll from './pages/QuestionPage.jsx'
 import './global.css'
+import './pages/navbar.css'
+import './pages/Icons.css'
+
 import { Route, Routes, Link } from 'react-router-dom'
 
 function App() {
   return (
-    <div className='phone-screen'> 
-     {<ul> 
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/search">Search</Link></li>
-        <li><Link to="/charity">Charity</Link></li>
-        <li><Link to="/poll">Poll</Link></li>
-    </ul>}
+    <div className='phone-screen'>
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/search" element={<Search />}></Route>
-        <Route path="/charity" element={<Charity />}></Route>
+        {/* <Route path="/charity" element={<Charity />}></Route> */}
         <Route path="/poll" element={<Poll />}></Route>
       </Routes>
+      <div class="nav">
+        <Link to="/"><button><span className="gg-home"></span ></button></Link>
+        <Link to="/search"><button><span className="gg-search"></span></button></Link>
+        <Link to="/poll"><button><span className="gg-clipboard"></span></button></Link>
+      </div>
     </div>
   )
 }
