8f0349d75c129539a20cdbce52a8f917d6b18a34
jasonx01 <jaysonlau2003@gmail.com>
2023-08-05 16:44:02 -0400

    started raffle page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Home from './pages/HomePage.jsx'
 import Charity from './pages/CharityPage.jsx'
 import Search from './pages/SearchPage.jsx'
 import Poll from './pages/QuestionPage.jsx'
+import Raffle from './pages/RafflePage.jsx'
 import './global.css'
 import './pages/navbar.css'
 import './pages/Icons.css'
@@ -15,16 +16,17 @@ function App() {
   return (
     <div className='phone-screen'>
       <Routes>
-        <Route path="/" element={<Home iguanaImage={Iguana} STCImg={STC}/>}></Route>
+        <Route path="/" element={<Home iguanaImage={Iguana} STCImg={STC} />}></Route>
         <Route path="/search" element={<Search />}></Route>
-        {/* <Route path="/charity" element={<Charity />}></Route> */}
         <Route path="/poll" element={<Poll />}></Route>
-        <Route path="/charitypage" element={<Charity iguanaImage={Iguana}/>}></Route>
+        <Route path="/charitypage" element={<Charity iguanaImage={Iguana} />}></Route>
+        <Route path="/raffle" element={<Raffle />}></Route>
       </Routes>
       <div class="nav">
         <Link to="/"><button className='navbutton'><span className="gg-home"></span ></button></Link>
         <Link to="/search"><button className='navbutton'><span className="gg-search"></span></button></Link>
         <Link to="/poll"><button className='navbutton'><span className="gg-clipboard"></span></button></Link>
+        <Link to="/raffle"><button className='navbutton'><span className="gg-smile"></span></button></Link>
       </div>
     </div>
   )
---
+++ src/pages/RafflePage.jsx
@@ -0,0 +1,3 @@
+export default function Raffles() {
+    return <h1>Hi</h1>
+}
