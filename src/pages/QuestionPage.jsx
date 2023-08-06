ee8bced8313ad176f0436bfd440ab59cbc99c3bf
William Lin <88000735+SoulSentinel@users.noreply.github.com>
2023-08-05 22:19:39 -0400

    Modified QuestionPage

--- src/pages/QuestionPage.jsx
+++ src/pages/QuestionPage.jsx
@@ -1,81 +1,77 @@
 import React, { useState, useEffect } from "react";
 import "./questions.css";
 
+function Question({ current, questions, selectedOption, setSelectedOption, companyList}) {
+  const question = questions[current];
+  const company = companyList[current];
+
+  return (
+    <>
+      <h2>Question: {current + 1} of 10</h2>
+      <h3 className="question-text">{question?.text}</h3>
+      <h2>Company: {company?.name}</h2>
+      <ul>
+        {question?.options.map(option => (
+          <li
+            key={option.id}
+            className={selectedOption === option ? "selected" : ""}
+            onClick={() => setSelectedOption(option)}
+          >
+            {option.text}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
 function Poll() {
-  const [showResults, setShowResults] = useState(false);
+  const [showResults, endPoll] = useState(false);
   const [currentQuestion, setCurrentQuestion] = useState(0);
-  const [score, setScore] = useState(0);
   const [questions, setQuestions] = useState([]);
   const [selectedOption, setSelectedOption] = useState(null);
-  const [responses, setResponses] = useState([]);
+  const [companyList, setCompanyList] = useState([]);
 
   useEffect(() => {
     fetch('/questions.json')
       .then(response => response.json())
-      .then(data => setQuestions(data))
-      .catch((error) => console.error("Error fetching questions:", error));
+      .then(data => setQuestions(data.sort(() => 0.5 - Math.random())))
+      .catch(error => console.error("Error fetching:", error));
+
+      fetch('/companyList.json')
+      .then(response => response.json())
+      .then(data => setCompanyList(data.sort(() => 0.5 - Math.random())))
+      .catch(error => console.error("Error fetching", error));
   }, []);
 
   const submitAnswer = () => {
-    if (selectedOption) {
-      setResponses([...responses, selectedOption.text]);
-      if (selectedOption.isCorrect) {
-        setScore(score + 1);
-      }
-      if (currentQuestion + 1 < 10) {
-        setCurrentQuestion(currentQuestion + 1);
-        setSelectedOption(null);
-      } else {
-        setShowResults(true);
-      }
+    if (!selectedOption) return;
+    if (currentQuestion + 1 < 10) {
+      setCurrentQuestion(prev => prev + 1);
+      setSelectedOption(null);
+    } else {
+      endPoll(true);
     }
   };
-
-  /*
-  const downloadResponses = () => {
-    const element = document.createElement("a");
-    const file = new Blob([responses.join("\n")], {type: 'text/plain'});
-    element.href = URL.createObjectURL(file);
-    element.download = "responses.txt";
-    document.body.appendChild(element);
-    element.click();
-  };
-  */
 
   return (
     <div className="Poll">
       {showResults ? (
-        <div style={{ textAlign: 'center' }}>
-          <h2>Done with all the questions today!</h2>
-        </div>
+        <h2>Done with all the questions today!</h2>
       ) : (
         <>
-          <h2>
-            Question: {currentQuestion + 1} out of {10}
-          </h2>
-          <h3 className="question-text">{questions[currentQuestion]?.text}</h3>
-          <p>
-            <h2>Company: {questions[currentQuestion]?.company}
-            </h2>
-            </p>  
-          <ul>
-            {questions[currentQuestion]?.options.map((option) => {
-              return (
-                <li
-                  key={option.id}
-                  className={selectedOption === option ? "selected" : ""}
-                  onClick={() => setSelectedOption(option)}
-                >
-                  {option.text}
-                </li>
-              );
-            })}
-          </ul>
+          <Question 
+            current={currentQuestion} 
+            questions={questions} 
+            selectedOption={selectedOption} 
+            setSelectedOption={setSelectedOption} 
+            companyList={companyList}
+          />
           <button className='submit' onClick={submitAnswer} style={{margin: '10px'}}>Submit</button>
-          </>
+        </>
       )}
     </div>
   );
-};
+}
 
 export default Poll;
